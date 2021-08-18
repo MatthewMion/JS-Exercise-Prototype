3bc2c7c46fc7157f59b32a2f3487d16fa538d12c
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-18 09:59:19 -0400

    test push

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function Airplane(name) {
 Airplane.prototype.takeOff = function () {
   this.isFlying = true;
 };
+
 Airplane.prototype.land = function () {
   this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,15 +39,7 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
-}
-
-
-
-
-
-
+function Person() {}
 
 /*
   TASK 2
@@ -63,10 +55,7 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
-}
-
+function Car() {}
 
 /*
   TASK 3
@@ -75,10 +64,7 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
-}
-
+function Baby() {}
 
 /* 
   TASK 4
@@ -89,18 +75,17 @@ function Baby() {
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working!');
-  return 'bar';
+function foo() {
+  console.log("its working!");
+  return "bar";
 }
 foo();
 module.exports = {
   foo,
-  Person, 
+  Person,
   Car,
-  Baby
-}
+  Baby,
+};
